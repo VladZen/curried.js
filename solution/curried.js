29546cb11d7556ea689f7fa62e73d55b25b67682
Vladislav Drannikov <vladislav.drannikov@justwebit.ru>
2016-10-14 15:07:06 +0300

    fixed syntax missings

--- solution/curried.js
+++ solution/curried.js
@@ -1,15 +1,15 @@
 function curried(func) {
-    if (typeof func !== 'function') return false
+    if (typeof func !== 'function') return false;
 
     const LENGTH = func.length;
 
     const NEXT = (args, ...new_args) => {
         args = [...args, ...new_args];
-        if (args.length < LENGTH) return NEXT.bind(null, args)
-        return func.apply(this, args)
+        if (args.length < LENGTH) return NEXT.bind(null, args);
+        return func.apply(this, args);
     }
 
-    return NEXT.bind(null, [])
+    return NEXT.bind(null, []);
 }
 
 module.exports = curried;
